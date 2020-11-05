3d6105ae0361f95864e373825bc179e46b597cea
BuildTools <unconfigured@null.spigotmc.org>
2020-11-05 13:43:52 +0100

    Updating MainApplication

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,3 +1,31 @@
-public class MainApplication {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+
+public class MainApplication extends Application {
+
+    public void start(Stage primaryStage) {
+        try {
+            URL url = new File("src/main/java/view/MainApplication.fxml").toURI().toURL();
+            Parent root = FXMLLoader.load(url);
+            primaryStage.setTitle("KarteiKarte KALEMARO");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+            primaryStage.setResizable(false);
+            Platform.setImplicitExit(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }
