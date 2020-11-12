6321a44b8846468363b85708f7ff777c9a77854d
leons <leonsoentgerath@web.de>
2020-11-12 14:35:03 +0100

    jup

---
+++ src/main/java/dxc/karteikarte/Main.java
@@ -0,0 +1,8 @@
+package dxc.karteikarte;
+
+public class Main {
+    public static void main(String[] args) {
+        MainApplication mainApplication = new MainApplication();
+        mainApplication.main(args);
+    }
+}
